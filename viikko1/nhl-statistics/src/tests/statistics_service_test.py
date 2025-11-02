ffab842557f7d5407e5691d9c7d868a38e68a582
Septicuss <vbalaban55@gmail.com>
2025-11-02 17:11:10 +0200

    test: test top functionality

--- viikko1/nhl-statistics/src/tests/statistics_service_test.py
+++ viikko1/nhl-statistics/src/tests/statistics_service_test.py
@@ -1,5 +1,5 @@
 import unittest
-from statistics_service import StatisticsService
+from statistics_service import StatisticsService, SortBy
 from player import Player
 
 class PlayerReaderStub:
@@ -32,9 +32,23 @@ class TestStatisticsService(unittest.TestCase):
         self.assertIsNotNone(result)
         self.assertTrue(len(result) == 3)
 
-    def test_top(self):
+    def test_top_by_points(self):
         result = self.stats.top(3)
         self.assertIsNotNone(result)
         self.assertEqual(result[0].points, 124)
         self.assertEqual(result[1].points, 99)
         self.assertEqual(result[2].points, 98)
+
+    def test_top_by_goals(self):
+        result = self.stats.top(3, SortBy.GOALS)
+        self.assertIsNotNone(result)
+        self.assertEqual(result[0].goals, 45)
+        self.assertEqual(result[1].goals, 42)
+        self.assertEqual(result[2].goals, 37)
+
+    def test_top_by_assists(self):
+        result = self.stats.top(3, SortBy.ASSISTS)
+        self.assertIsNotNone(result)
+        self.assertEqual(result[0].assists, 89)
+        self.assertEqual(result[1].assists, 56)
+        self.assertEqual(result[2].assists, 54)
