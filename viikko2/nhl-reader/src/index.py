f136c4c98c97cf9b2a8a6d9476e6996949aa528a
Septicuss <vbalaban55@gmail.com>
2025-11-10 17:45:07 +0200

    feat(w2): finish nhl-reader

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,41 @@
-from player import Player
+from rich.console import Console
+from rich.prompt import Prompt
+from rich.table import Table
+
+from player import PlayerReader
+from player import PlayerStats
+
+console = Console()
 
 def main():
-    pass
+    seasons = ["2018-19", "2019-20", "2020-21", "2021-22", "2022-23", "2023-24", "2024-25", "2025-26"]
+    season = Prompt.ask(prompt="Season", choices=seasons, default="2024-25")
+
+    url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+
+    while True:
+        nationality = Prompt.ask(prompt="Nationality", choices=stats.nationalities())
+        if not nationality or nationality.isspace():
+            continue
+
+        players = stats.top_scorers_by_nationality(nationality)
+
+        table = Table(title=f"Season {season} players from {nationality}")
+        table.add_column("Released", style="cyan")
+        table.add_column("teams", style="magenta")
+        table.add_column("goals", style="green", justify="right")
+        table.add_column("assists", style="green", justify="right")
+        table.add_column("points", style="green", justify="right")
+
+        amount = 1
+        for player in players:
+            table.add_row(player.name, player.team, str(player.goals), str(player.assists), str(player.score()))
+            if amount >= 12: break
+            else: amount += 1
+
+        console.print(table)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,42 @@
+import requests
+
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.nationality = dict['nationality']
+        self.assists = int(dict['assists'])
+        self.goals = int(dict['goals'])
+        self.team = dict['team']
+        self.games = int(dict['games'])
+
+    def score(self):
+        return self.goals + self.assists
     
     def __str__(self):
-        return self.name
+        return f"{self.name:20} {self.team:15} {self.goals} + {self.assists} = {self.goals+self.assists}"
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.players = reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality: str):
+        filtered = [ player for player in self.players if player.nationality == nationality ]
+        return sorted(filtered, key=lambda player: player.score(), reverse=True)
+
+    def nationalities(self):
+        return set(player.nationality for player in self.players)
+
+class PlayerReader:
+
+    def __init__(self, url: str):
+        self._url = url
+
+    def get_players(self):
+        players = []
+
+        response = requests.get(self._url).json()
+        for player_dict in response:
+            player = Player(player_dict)
+            players.append(player)
+
+        return players
