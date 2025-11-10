4181ac9d66cdc7f2751c4de435da286cfe26f596
Septicuss <vbalaban55@gmail.com>
2025-11-10 18:34:36 +0200

    refactor(w2): fix pylint errors

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,17 +1,17 @@
 import requests
 
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
-        self.nationality = dict['nationality']
-        self.assists = int(dict['assists'])
-        self.goals = int(dict['goals'])
-        self.team = dict['team']
-        self.games = int(dict['games'])
+    def __init__(self, dictionary):
+        self.name = dictionary['name']
+        self.nationality = dictionary['nationality']
+        self.assists = int(dictionary['assists'])
+        self.goals = int(dictionary['goals'])
+        self.team = dictionary['team']
+        self.games = int(dictionary['games'])
 
     def score(self):
         return self.goals + self.assists
-    
+
     def __str__(self):
         return f"{self.name:20} {self.team:15} {self.goals} + {self.assists} = {self.goals+self.assists}"
 
@@ -31,10 +31,13 @@ class PlayerReader:
     def __init__(self, url: str):
         self._url = url
 
+    def get_url(self):
+        return self._url
+
     def get_players(self):
         players = []
 
-        response = requests.get(self._url).json()
+        response = requests.get(self._url, timeout=60).json()
         for player_dict in response:
             player = Player(player_dict)
             players.append(player)
