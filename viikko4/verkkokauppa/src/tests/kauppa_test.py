67488713b1f2436ac4f9c5221de8ca57f0b6b08a
Septicuss <vbalaban55@gmail.com>
2025-11-25 01:40:24 +0200

    test: implement more kauppa tests

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -7,6 +7,7 @@ from tuote import Tuote
 
 class TestKauppa(unittest.TestCase):
     def setUp(self):
+        self.ostoskori_mock = Mock()
         self.pankki_mock = Mock()
         self.varasto_mock = Mock()
         self.viitegeneraattori_mock = Mock()
@@ -65,4 +66,26 @@ class TestKauppa(unittest.TestCase):
         self.kauppa.tilimaksu("pekka", "12345")
         self.pankki_mock.tilisiirto.assert_called_with("pekka", 42, "12345", self.kauppa._kaupan_tili, 5)
 
-    
+    def test_uusi_asiointi_nollaa_ostokset(self):
+        self.kauppa._ostoskori = self.ostoskori_mock
+        self.kauppa.aloita_asiointi()
+        self.ostoskori_mock.assert_not_called()
+
+    def test_viitenumero_pyydetaan_joka_kerta(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("pekka", "12345")
+        self.viitegeneraattori_mock.uusi.assert_called()
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("pekka", "12345")
+        self.viitegeneraattori_mock.uusi.assert_called()
+
+    def test_korista_poistaminen_toimii(self):
+        tuote = self.varasto_mock.hae_tuote(1)
+        self.kauppa.aloita_asiointi()
+        self.kauppa._ostoskori = self.ostoskori_mock
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.poista_korista(1)
+        self.varasto_mock.palauta_varastoon.assert_called_with(tuote)
+        self.ostoskori_mock.poista.assert_called_with(tuote)
