a44cb958bf13d580b727be3a595dcdb42f3cef20
Septicuss <vbalaban55@gmail.com>
2025-12-05 13:39:46 +0200

    feat: query language improvements 1

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or, QueryBuilder
 
 
 def main():
@@ -8,11 +8,14 @@ def main():
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasFewerThan(2, "goals"),
-        PlaysIn("NYR")
+    query = QueryBuilder()
+    matcher = (
+        query
+        .plays_in("NYR")
+        .has_at_least(10, "goals")
+        .has_fewer_than(20, "goals")
+        .build()
     )
-
     for player in stats.matches(matcher):
         print(player)
 
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -9,6 +9,15 @@ class And:
 
         return True
 
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+        return False
 
 class PlaysIn:
     def __init__(self, team):
@@ -47,3 +56,28 @@ class HasFewerThan(Compare):
         player_value = getattr(player, self._attr)
 
         return player_value < self._value
+
+
+class QueryBuilder:
+    def __init__(self, matcher = All()):
+        self._matcher = matcher
+
+    def plays_in(self, team):
+        return self._with(PlaysIn(team))
+
+    def has_at_least(self, value, attr):
+        return self._with(HasAtLeast(value, attr))
+
+    def all(self):
+        return self._with(All())
+
+    def has_fewer_than(self, value, attr):
+        return self._with(HasFewerThan(value, attr))
+
+    def _with(self, matcher):
+        return QueryBuilder(And(self._matcher, matcher))
+
+    def build(self):
+        return self._matcher
+
+    pass
